3ec329ad70c1b70fe99654e51eedb6733bf74357
apere0695 <aaalan0695@gmail.com>
2025-04-08 14:55:05 -0300

    configure gradle

--- lib/core/injection/service_locator.dart
+++ lib/core/injection/service_locator.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
+import 'package:practica_hexagonal_bloc/core/client/http_client.dart';
 import 'package:practica_hexagonal_bloc/data/datasources/task_api_service.dart';
 import 'package:practica_hexagonal_bloc/data/repositories_implements/task_repository_implements.dart';
 import 'package:practica_hexagonal_bloc/domain/repositories/task_repositories.dart';
@@ -10,6 +11,8 @@ final sl = GetIt.instance;
 void setupLocator() {
   // Inyectamos http.Client
   sl.registerLazySingleton(() => http.Client());
+
+  sl.registerLazySingleton<HttpClient>(() => HttpClientImpl(sl<http.Client>()));
 
   // Inyectamos el servicio API
   sl.registerLazySingleton<TaskApiService>(() => TaskApiServiceImpl(sl()));
